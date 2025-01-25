d991b82671fbda578fef8b822652e22cb59db5d7
AdityaChellam <y.s.aditya123@gmail.com>
2025-01-25 16:07:41 +0100

    shopping cart example page

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("world")
+document.addEventListener("DOMContentLoaded", () => {
+    const bars = document.querySelectorAll(".bar-inner");
+  
+    // Trigger the animation on page load
+    bars.forEach((bar) => {
+      const duration = getComputedStyle(bar).getPropertyValue("--duration");
+      bar.style.animationDuration = duration;
+    });
+  });
+  
